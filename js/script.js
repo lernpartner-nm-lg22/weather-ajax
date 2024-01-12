c9537c499ab83fd802648485aea3d20d4eb0d473
Johannes Kingma <j.kingma@sbw.edu>
2024-01-12 11:58:46 +0100

    add keypress

--- js/script.js
+++ js/script.js
@@ -15,24 +15,34 @@ class Weather {
    */
   init() {
     this.elementOkButton = document.getElementById('ok');
-    this.elemenCity = document.getElementById('city');
+    this.elementCity = document.getElementById('city');
     this.elementTemp = document.getElementById('temp');
     this.elementHumidity = document.getElementById('humidity');
+    this.elementCloud = document.getElementById('cloud')
+    this.elementWind = document.getElementById('wind')
 
     this.elementOkButton.addEventListener('click', event => this.doOkClick(event));
+    this.elementCity.addEventListener('keypress', event=> this.doKeypress(event))
   }
   /**
    * Function that handles the click event
    * @param {*} event 
    */
   async doOkClick(event) {
-    const city = this.elemenCity.value;
+    const city = this.elementCity.value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.appId}`;
     const response = await fetch(url);
     const data = await response.json();
 
     this.elementTemp.textContent = data.main.temp;
+    this.elementHumidity.textContent = data.main.humidity;
+    this.elementCloud.textContent = dete.weather[0].description;
+    this.elementWind.textContent = data.wind.speed;
 
+  }
+  doKeyPress(event) {
+    if(Event.key = "Enter")
+    this.doOkClick(event);
   }
 
 }
