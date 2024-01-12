f92f1c2c26b0e92af1cda105fdf55363e0e8b80c
Johannes Kingma <50140049+kingma-sbw@users.noreply.github.com>
2024-01-12 08:34:25 +0000

    Initial

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+class Weather {
+  /**
+   * constructor of the Weather class, called when creating an object
+   * store construction members
+   */
+  constructor() {
+    this.init();
+  }
+
+  /**
+   * init function, connect the elements to JavaScript vars and create event handlers
+   */
+  init() {
+
+  }
+  /**
+   * Function that handles the click event
+   * @param {*} event 
+   */
+  async doOkClick(event) {
+
+  }
+
+}
+
+const weather = new Weather();
