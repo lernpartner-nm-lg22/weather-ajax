12d352090fd56e7efb0afc896c2b8793963feb8c
Johannes Kingma <j.kingma@sbw.edu>
2024-01-12 11:43:08 +0100

    first result

--- js/script.js
+++ js/script.js
@@ -5,24 +5,36 @@ class Weather {
    * constructor of the Weather class, called when creating an object
    * store construction members
    */
-  constructor() {
+  constructor(appId) {
     this.init();
+    this.appId = appId;
   }
 
   /**
    * init function, connect the elements to JavaScript vars and create event handlers
    */
   init() {
+    this.elementOkButton = document.getElementById('ok');
+    this.elemenCity = document.getElementById('city');
+    this.elementTemp = document.getElementById('temp');
+    this.elementHumidity = document.getElementById('humidity');
 
+    this.elementOkButton.addEventListener('click', event => this.doOkClick(event));
   }
   /**
    * Function that handles the click event
    * @param {*} event 
    */
   async doOkClick(event) {
+    const city = this.elemenCity.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.appId}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    this.elementTemp.textContent = data.main.temp;
 
   }
 
 }
 
-const weather = new Weather();
+const weather = new Weather("34357060115ac4f3b93708499ef4443c");
